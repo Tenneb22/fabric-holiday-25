d0c0fbed7187c4a03820586f6913014b6161e824
haykam821 <24855774+haykam821@users.noreply.github.com>
2025-12-19 22:27:48 -0500

    Use data generation for banner pattern tags

--- mod/src/datagen/java/holiday/DatagenEntrypoint.java
+++ mod/src/datagen/java/holiday/DatagenEntrypoint.java
@@ -11,6 +11,7 @@ public class DatagenEntrypoint implements DataGeneratorEntrypoint {
         FabricDataGenerator.Pack pack = dataGenerator.createPack();
 
         pack.addProvider(HolidayServerBannerPatternProvider::new);
+        pack.addProvider(HolidayServerBannerPatternTagProvider::new);
         pack.addProvider(HolidayServerBlockLootTableProvider::new);
         pack.addProvider(HolidayServerBlockTagProvider::new);
         pack.addProvider(HolidayServerPaintingVariantProvider::new);
