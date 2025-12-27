8b3d8447bbf913dc3a1250fff9ef0b88a1bfac2a
JumperOnJava <59884606+JumperOnJava@users.noreply.github.com>
2025-12-27 14:28:44 +0200

    fixed model

--- mod/src/main/java/holiday/baritone/fabric_register_thing_stolen_from_tutorial.java
+++ mod/src/main/java/holiday/baritone/fabric_register_thing_stolen_from_tutorial.java
@@ -1,6 +1,7 @@
 package holiday.baritone;
 
-import holiday.CommonEntrypoint;import net.minecraft.item.Item;
+import holiday.CommonEntrypoint;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;import net.minecraft.registry.RegistryKeys;import net.minecraft.util.Identifier;import java.util.function.Function;
@@ -10,7 +11,7 @@ public class fabric_register_thing_stolen_from_tutorial {
         Identifier айді = Identifier.of(CommonEntrypoint.MOD_ID, ідентифікатор);
         RegistryKey<Item> ключ = RegistryKey.of(RegistryKeys.ITEM, айді);
 
-        Item.Settings налаштування = new Item.Settings().registryKey(ключ);
+        Item.Settings налаштування = new Item.Settings().registryKey(ключ).maxCount(1);
         T предмет = factory.apply(налаштування);
 
         return Registry.register(Registries.ITEM, ключ, предмет);
